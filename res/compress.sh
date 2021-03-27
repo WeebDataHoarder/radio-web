3bc6668baa4637646179324303817275e7c445bc
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2021-03-27 22:55:31 +0100

    updated dict with better compression

--- res/compress.sh
+++ res/compress.sh
@@ -6,6 +6,11 @@ pushd "${1}" || exit
 
 for f in *.@(js|mjs|data|wasm|mem|css|map|svg|ttf|woff|woff2); do
   echo "compressing $f"
-  zopfli --gzip -c --verbose --i50 "$f" > "$f.gz"
+  if [[ "$f" == *data ]]; then
+    #Use not so slow compression on the large binary files
+    gzip --best --stdout --keep "$f" > "$f.gz"
+  else
+    zopfli --gzip -c --verbose --i50 "$f" > "$f.gz"
+  fi
   brotli --best --stdout --keep --no-copy-stat "$f" > "$f.br"
 done
